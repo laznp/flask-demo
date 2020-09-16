78c587e5e30f5da2c3c0285a30bfd7acc6302ffe
Lazuardi N Putra <lazuardi.putra@tokopedia.com>
2020-09-16 23:42:46 +0700

    testing wehbook

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ app.secret_key = "SecretKey!"
 @app.route("/")
 def root():
 	return "Root"
-
-@app.route("/hello")
-def hello():
-	return "Hello"
     
 if __name__ == '__main__':
 	app.run(debug=True)
